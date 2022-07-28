1128d6acb4e6fc4ff45bf23dbccc82435f29f9be
Dinesh Pinto <d.pinto@fkf.mpg.de>
2022-07-28 18:24:41 +0200

    IOHandler now contains only staticmethods

--- src/io_handler.py
+++ src/io_handler.py
@@ -4,12 +4,38 @@ import inspect
 import itertools
 import os
 import pickle
-from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from dateutil import parser
+
+
+def _check_extension(filepath: str, extension: str) -> str:
+    """
+    Check if filepath extension matches expected extension.
+    - If no extension found, add extension
+    - If wrong extension found, raise IOError
+    """
+    filename = os.path.basename(filepath)
+
+    if filename.endswith(extension):
+        return filepath
+    elif "." in filename:
+        _, ext = filename.split(".")
+        raise IOError(f"Invalid extension '{ext}' in '{filename}', extension should be '{extension}'")
+    else:
+        filepath += extension
+    return filepath
+
+
+def _channel_to_gauge_names(channel_names: list) -> list:
+    """ Replace the channel names with gauge locations. """
+    gauges = {"CH 1": "Main", "CH 2": "Prep", "CH 3": "Backing"}
+    try:
+        return [gauges[ch] for ch in channel_names]
+    except KeyError:
+        return channel_names
 
 
 class IOHandler:
@@ -60,65 +86,53 @@ class IOHandler:
         return np.genfromtxt(filepath, **kwargs)
 
     @staticmethod
-    def __check_extension(filepath: str, extension: str) -> str:
-        """
-        Check if filepath extension matches expected extension.
-        - If no extension found, add extension
-        - If wrong extension found, raise IOError
-        """
-        filename = os.path.basename(filepath)
-
-        if filename.endswith(extension):
-            return filepath
-        elif "." in filename:
-            _, ext = filename.split(".")
-            raise IOError(f"Invalid extension '{ext}' in '{filename}', extension should be '{extension}'")
-        else:
-            filepath += extension
-        return filepath
-
     def load_pys(self, filepath: str) -> np.ndarray:
         """ Loads raw pys data files. Wraps around numpy.load. """
-        filepath = self.__check_extension(filepath, ".pys")
+        filepath = _check_extension(filepath, ".pys")
         return np.load(filepath, encoding="bytes", allow_pickle=True)
 
-    def save_pys(self, dictionary: dict, filepath: str):
+    @staticmethod
+    def save_pys(dictionary: dict, filepath: str):
         """ Saves processed pickle files for plotting/further analysis. """
         if not os.path.exists(filepath):
             os.makedirs(filepath)
-        filepath = self.__check_extension(filepath, ".pys")
+        filepath = _check_extension(filepath, ".pys")
 
         with open(filepath, 'wb') as f:
             pickle.dump(dictionary, f, 1)
 
-    def save_df(self, df: pd.DataFrame, filepath: str):
+    @staticmethod
+    def save_df(df: pd.DataFrame, filepath: str):
         """ Save Dataframe as csv. """
         if not os.path.exists(filepath):
             os.makedirs(filepath)
-        filepath = self.__check_extension(filepath, ".csv")
+        filepath = _check_extension(filepath, ".csv")
 
         df.to_csv(filepath, sep='\t', encoding='utf-8')
 
-    def load_pkl(self, filepath: str) -> dict:
+    @staticmethod
+    def load_pkl(filepath: str) -> dict:
         """ Loads processed pickle files for plotting/further analysis. """
-        filepath = self.__check_extension(filepath, ".pkl")
+        filepath = _check_extension(filepath, ".pkl")
 
         with open(filepath, 'rb') as f:
             file = pickle.load(f)
         return file
 
-    def save_pkl(self, obj: object, filepath: str):
+    @staticmethod
+    def save_pkl(obj: object, filepath: str):
         """ Saves processed pickle files for plotting/further analysis. """
         if not os.path.exists(filepath):
             os.makedirs(filepath)
-        filepath = self.__check_extension(filepath, ".pkl")
+        filepath = _check_extension(filepath, ".pkl")
 
         with open(filepath, 'wb') as f:
             pickle.dump(obj, f)
 
-    def read_nanonis_data(self, filepath: str) -> pd.DataFrame:
+    @staticmethod
+    def read_nanonis_data(filepath: str) -> pd.DataFrame:
         """ Extract data from a Nanonis dat file. """
-        filepath = self.__check_extension(filepath, ".dat")
+        filepath = _check_extension(filepath, ".dat")
 
         skip_rows = 0
         with open(filepath) as dat_file:
@@ -134,9 +148,10 @@ class IOHandler:
         df = pd.read_table(filepath, sep="\t", skiprows=skip_rows)
         return df
 
-    def read_nanonis_parameters(self, filepath: str) -> dict:
+    @staticmethod
+    def read_nanonis_parameters(filepath: str) -> dict:
         """ Extract parameters from a Nanonis dat file. """
-        filepath = self.__check_extension(filepath, ".dat")
+        filepath = _check_extension(filepath, ".dat")
 
         parameters = {}
         with open(filepath) as dat_file:
@@ -159,26 +174,15 @@ class IOHandler:
                     parameters[label] = value
         return parameters
 
-    def read_nanonis_params_and_data(self, filepath: str) -> Tuple[dict, pd.DataFrame]:
-        """ Convenience function to extract both parameters and data from a Nanonis .dat file. """
-        return self.read_nanonis_parameters(filepath), self.read_nanonis_data(filepath)
-
-    def _channel_to_gauge_names(self, channel_names: list) -> list:
-        """ Replace the channel names with gauge locations. """
-        gauges = {"CH 1": "Main", "CH 2": "Prep", "CH 3": "Backing"}
-        try:
-            return [gauges[ch] for ch in channel_names]
-        except KeyError:
-            return channel_names
-
-    def read_pfeiffer_data(self, filepath: str) -> pd.DataFrame:
+    @staticmethod
+    def read_pfeiffer_data(filepath: str) -> pd.DataFrame:
         """ Read data stored by Pfeiffer vacuum monitoring software. """
-        filepath = self.__check_extension(filepath, ".txt")
+        filepath = _check_extension(filepath, ".txt")
 
         # Extract only the header to check which gauges are connected
         file_header = pd.read_csv(filepath, sep="\t", skiprows=1, nrows=1)
 
-        header = self._channel_to_gauge_names(file_header)
+        header = _channel_to_gauge_names(file_header)
 
         # Create DataFrame with new header
         df = pd.read_csv(filepath, sep="\t", skiprows=5, names=header)
@@ -188,9 +192,10 @@ class IOHandler:
         df = df.drop(['Date', 'Time'], axis=1)
         return df
 
-    def read_lakeshore_data(self, filepath: str) -> pd.DataFrame:
+    @staticmethod
+    def read_lakeshore_data(filepath: str) -> pd.DataFrame:
         """ Read data stored by Lakeshore temperature monitor software. """
-        filepath = self.__check_extension(filepath, ".xls")
+        filepath = _check_extension(filepath, ".xls")
 
         # Extract only the timestamp
         timestamp = pd.read_excel(filepath, skiprows=1, nrows=1, usecols=[1], header=None)[1][0]
@@ -204,9 +209,10 @@ class IOHandler:
         df["Datetime"] = [timestamp_dt + datetime.timedelta(milliseconds=ms) for ms in df["Time"]]
         return df
 
-    def read_oceanoptics_data(self, filepath: str) -> pd.DataFrame:
+    @staticmethod
+    def read_oceanoptics_data(filepath: str) -> pd.DataFrame:
         """ Read spectrometer data from OceanOptics spectrometer. """
-        filepath = self.__check_extension(filepath, ".txt")
+        filepath = _check_extension(filepath, ".txt")
 
         df = pd.read_csv(filepath, sep="\t", skiprows=14, names=["wavelength", "intensity"])
         return df
