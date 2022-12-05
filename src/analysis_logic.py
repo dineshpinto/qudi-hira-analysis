c5ef602dc468f21df85ba03c034cd0ca606a1be6
Dinesh Pinto <d.pinto@fkf.mpg.de>
2022-12-05 20:16:03 +0100

    Added estimators to perform_fit() function

--- src/analysis_logic.py
+++ src/analysis_logic.py
@@ -77,6 +77,9 @@ class AnalysisLogic(FitLogic):
             |           | sinetriplewithexpdecay        |
             |           | sinetriplewiththreeexpdecay   |
             | 2d        | twoDgaussian                  |
+        Estimators:
+            - generic
+            - dip
         """
 
         if isinstance(x, pd.Series) or isinstance(x, pd.Index):
