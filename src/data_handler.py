24f5034ad1579f2e04b46fd9f611c16c559e3eb7
Dinesh Pinto <d.pinto@fkf.mpg.de>
2023-01-06 06:35:02 +0100

    Added tree visualization support and ability to create different output structure
    Signed-off-by: Dinesh Pinto <d.pinto@fkf.mpg.de>

--- src/data_handler.py
+++ src/data_handler.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import datetime
 import logging
 import os
+from pathlib import Path
 from typing import List, TYPE_CHECKING, Callable
 
 from src.io_handler import IOHandler
@@ -18,7 +19,13 @@ logging.basicConfig(format='%(name)s :: %(levelname)s :: %(message)s', level=log
 
 
 class DataHandler(IOHandler):
-    def __init__(self, data_folder: str, figure_folder: str, measurement_folder: str = None):
+    def __init__(
+            self,
+            data_folder: str,
+            figure_folder: str,
+            measurement_folder: str,
+            copy_measurement_folder_structure: bool = True
+    ):
         """
         This class is specific for qudi-hira measurements. To support adding new measurement types to the dataclass
         the callable needs to be added into `DataLoader` and corresponding functions need to be added into
@@ -29,7 +36,10 @@ class DataHandler(IOHandler):
         """
         self.log = logging.getLogger(__name__)
         self.data_folder_path = self.__get_data_folder_path(data_folder, measurement_folder)
-        self.figure_folder_path = self.__get_figure_folder_path(figure_folder, measurement_folder)
+        if copy_measurement_folder_structure:
+            self.figure_folder_path = self.__get_figure_folder_path(figure_folder, measurement_folder)
+        else:
+            self.figure_folder_path = figure_folder
 
         super().__init__(base_read_path=self.data_folder_path, base_write_path=self.figure_folder_path)
 
@@ -77,6 +87,39 @@ class DataHandler(IOHandler):
         else:
             self.log.info(f"Figure folder path is {path}")
         return path
+
+    def __tree(self, dir_path: Path, prefix: str = ''):
+        """
+        A recursive generator, given a directory Path object
+        will yield a visual tree structure line by line
+        with each line prefixed by the same characters
+        """
+        # prefix components:
+        space = '    '
+        branch = '│   '
+        # pointers:
+        tee = '├── '
+        last = '└── '
+
+        contents = list(dir_path.iterdir())
+        # contents each get pointers that are ├── with a final └── :
+        pointers = [tee] * (len(contents) - 1) + [last]
+        for pointer, path in zip(pointers, contents):
+            yield prefix + pointer + path.name
+            if path.is_dir():  # extend the prefix and recurse:
+                extension = branch if pointer == tee else space
+                # i.e. space because last, └── , above so no more |
+                yield from self.__tree(path, prefix=prefix + extension)
+
+    def data_folder_tree(self):
+        """ Print a tree of the data folder. """
+        for line in self.__tree(Path(self.data_folder_path)):
+            print(line)
+
+    def figure_folder_tree(self):
+        """ Print a tree of the figure folder. """
+        for line in self.__tree(Path(self.figure_folder_path)):
+            print(line)
 
     def get_measurement_filepaths(self, measurement: str, extension: str = ".dat",
                                   exclude_str: str = "image_1.dat") -> list:
