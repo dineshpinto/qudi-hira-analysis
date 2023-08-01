a6603c076c40b0bdd206615f2d29bc3685c68efb
dineshpinto <annual.fallout_0z@icloud.com>
2023-08-01 02:04:58 +0200

    Added FitMethodsAndEstimators class to init, updated .gitignore

--- qudi_hira_analysis/__init__.py
+++ qudi_hira_analysis/__init__.py
@@ -1,5 +1,5 @@
-from .analysis_logic import AnalysisLogic
+from .analysis_logic import AnalysisLogic, FitMethodsAndEstimators
 from .data_handler import DataHandler
 from .io_handler import IOHandler
 
-__all__ = ["DataHandler", "IOHandler", "AnalysisLogic"]
+__all__ = ["DataHandler", "IOHandler", "AnalysisLogic", "FitMethodsAndEstimators"]
